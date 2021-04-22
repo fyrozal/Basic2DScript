7bf0f8b5fd9818abcdc2a89b4c4d3b8e8a6e84b4
Zulhaydar Akbar <fyrozal.akbar@gmail.com>
2021-04-22 12:21:51 +0700

    Initial commit

---
+++ Assets/Script/Latihan.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Latihan : MonoBehaviour
+
+{
+
+
+    // Start is called before the first frame update
+
+  
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log("ini Update");
+
+    }
+
+    void Awake()
+    {
+        Debug.Log("Awake dipanggil");
+    }
+
+    void Start()
+    {
+        Debug.Log("ini start");
+    }
+
+    void FixedUpdate()
+    {
+        Debug.Log("Waktu untuk FixedUpdate :" + Time.deltaTime);
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# Basic2DScript
+ 
